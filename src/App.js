e9cc2553de86750b70f16fed9ff037d8c6824c1e
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-12-07 08:20:32 -0500

    feat: checkpoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> My App </h1>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Body = () => {
+    return (
+        <div>
+            <h3> Body </h3>
+        </div>
+    )
+}
+
+export default Body

4fd29ae6c7e7712e8f59f32c2df0d5495da0c30d
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-12-22 15:37:47 -0500

    checkpoint

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,81 +6,90 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarToggler, Collapse, Bu
 const Header = () => {
     return (
         <>
-            <div className='row'
+            <div className="header"
                 style={{
-                    position: 'absolute',
-                    left: '0',
-                    width: '100%',
-                    overflow: 'hidden'
-                }}>
-                <div className="col">
-                    <div className="navbar navbar-expand-lg navbar-light bg-light"
-                        expand="lg"
-                        dark sticky="top"
-                        style={{
-                            sticky: "top",
-                            width: "100%",
-                            position: "fixed",
-                            top: 0,
-                            backgroundColor: 'MidnightBlue',
-                        }}>
-                        <text style={{
-                            fontSize: "xx-large",
-                            align: "center",
-                            color: 'yellow',
-                            fontWeight: 'bold',
-                            font: 'Kalam',
-                            fontFamily: 'cursive',
-                            fontStyle: 'italic',
-                            paddingTop: '10px',
-                            paddingBottom: '30px',
-                            marginBottom: '20px'
-                        }}>
-                            COMMUNITY KANBAN
-                        </text>
-                        <br>
+                    flex: 1,
+                    marginTop: '10px',
+                    marginBottom: '10px',
+                    paddingTop: '10px',
+                    paddingBottom: '10px',
+                }}
+            >
+                <div className='row'
+                    style={{
+                        position: 'absolute',
+                        left: '0',
+                        width: '100%',
+                        overflow: 'hidden'
+                    }}>
+                    <div className="col">
+                        <div className="navbar navbar-expand-lg navbar-light bg-light"
+                            expand="lg"
+                            dark sticky="top"
+                            style={{
+                                sticky: "top",
+                                width: "100%",
+                                position: "fixed",
+                                top: 0,
+                                backgroundColor: 'MidnightBlue',
+                            }}>
+                            <text style={{
+                                fontSize: "xx-large",
+                                align: "center",
+                                color: 'yellow',
+                                fontWeight: 'bold',
+                                font: 'Kalam',
+                                fontFamily: 'cursive',
+                                fontStyle: 'italic',
+                                paddingTop: '10px',
+                                paddingBottom: '30px',
+                                marginBottom: '20px'
+                            }}>
+                                COMMUNITY KANBAN
+                            </text>
+                            <br>
 
-                        </br>
-                        <text style={{
-                            fontSize: "x-large",
-                            align: "center",
-                            color: 'white',
-                            font: 'Arial',
-                            paddingTop: '10px',
-                            paddingBottom: '10px',
-                            marginTop: '50px',
-                        }}>
-                            You can't spell Communi
-                        </text>
-                        <text style={{
-                            fontSize: "x-large",
-                            align: "center",
-                            color: 'yellow',
-                            font: 'cougard',
-                            fontStyle: 'italic',
-                            fontFamily: 'cursive',
-                            paddingTop: '10px',
-                            paddingBottom: '10px',
-                            marginTop: '50px',
-                        }}>
-                            IT
-                        </text>
-                        <text style={{
-                            fontSize: "x-large",
-                            align: "center",
-                            color: 'white',
-                            font: 'Arial',
-                            paddingTop: '10px',
-                            paddingBottom: '10px',
-                            marginTop: '50px',
-                        }}>
-                            y without I.T.
-                        </text>
+                            </br>
+                            <text style={{
+                                fontSize: "x-large",
+                                align: "center",
+                                color: 'white',
+                                font: 'Arial',
+                                paddingTop: '10px',
+                                paddingBottom: '10px',
+                                marginTop: '50px',
+                            }}>
+                                You can't spell Communi
+                            </text>
+                            <text style={{
+                                fontSize: "x-large",
+                                align: "center",
+                                color: 'yellow',
+                                font: 'cougard',
+                                fontStyle: 'italic',
+                                fontFamily: 'cursive',
+                                paddingTop: '10px',
+                                paddingBottom: '10px',
+                                marginTop: '50px',
+                            }}>
+                                IT
+                            </text>
+                            <text style={{
+                                fontSize: "x-large",
+                                align: "center",
+                                color: 'white',
+                                font: 'Arial',
+                                paddingTop: '10px',
+                                paddingBottom: '10px',
+                                marginTop: '50px',
+                            }}>
+                                y without I.T.
+                            </text>
 
-                    </div >
+                        </div >
+                    </div>
                 </div>
             </div>
-
         </>
     )
 };

3a986ac523abc1b179c646ff23aec080feb080e8
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-12-23 08:41:40 -0500

    feat: Routing enabled, with errors

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { Connect, Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import Header from './HeaderComponent';
+import { Connect, Switch, Routes, Route, Redirect, withRouter } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Contact from '../pages/Contact';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 class Main extends Component {
     render() {
         return (
             <div>
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path="/contact" component={Contact} />
-                    <Redirect to="/" />
-                </Switch>
-            </div>
+                <Header />
+                <BrowserRouter>
+                    <Switch>
+                        <Route path='/home' component={Home} />
+                        <Route exact path='/contact' component={Contact} />
+                        <Redirect to="/" />
+                    </Switch>
+                </BrowserRouter>
+            </div >
         );
     }
 }
 
-export default withRouter(Main);
+export default Main;

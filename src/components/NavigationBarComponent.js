9b0b52fe16ef126d0d1bf9f50c2b98b9e945341a
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-12-26 17:52:28 -0500

    feat: fixed navbar fonts

--- src/components/NavigationBarComponent.js
+++ src/components/NavigationBarComponent.js
@@ -1,18 +1,38 @@
 
 import React, { Component } from 'react';
 import { Card, CardImg } from 'reactstrap';
+import { div } from 'react-router-dom';
 
 class NavigationBar extends Component {
     render() {
         return (
             <div>
                 <ul id="nav">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Technologists</a></li>
-                    <li><a href="#">Nonprofits</a></li>
-                    <li><a href="#">Wiki</a></li>
+                    <div>
+                        <div className="nav-link" to="/home"
+                            border="1px solid white"
+                        >
+                            <i className="fa fa-home fa-lg" /> Home
+                        </div>
+                    </div>
+                    <div>
+                        <div className="nav-link" to="/directory">
+                            <i className="fa fa-list fa-lg" /> Directory
+                        </div>
+                    </div>
+                    <div>
+                        <div className="nav-link" to="/aboutus">
+                            <i className="fa fa-info fa-lg" /> About
+                        </div>
+                    </div>
+                    <div>
+                        <div className="nav-link" to="/contactus">
+                            <i className="fa fa-address-card fa-lg" /> Contact Us
+                        </div>
+                    </div>
+                    <div className="nav-link" to="/wiki">
+                        <i className="fa fa-address-card fa-lg" /> Wiki
+                    </div>
                 </ul>
             </div>
         );
